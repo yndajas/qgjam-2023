ffb57fa27474dc44216902829aea654debc53eaf
Ynda Jas <yndajas@gmail.com>
2023-10-21 22:22:35 +0100

    Add controls text
    This might be moved or removed depending on what I end up adding

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,6 +5,7 @@ var enemy_scene: PackedScene = preload("res://scenes/enemy.tscn")
 var flag_scene: PackedScene = preload("res://scenes/flag.tscn")
 var uncollected_flags: Array[int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 var score: int = 0
+@onready var controls_text: RichTextLabel = $InformationPanel/ControlsText
 @onready var converts_count_text: RichTextLabel = $InformationPanel/ConvertsCountText
 @onready var enemy_spawn_timer: Timer = $EnemySpawnTimer
 @onready var information_panel: ColorRect = $InformationPanel
@@ -59,6 +60,7 @@ func prepare_information_panel() -> void:
 	information_panel.size.x = Global.PLAYABLE_LEFT_EDGE
 	for panel_child in information_panel.get_children():
 		panel_child.size.x = Global.PLAYABLE_LEFT_EDGE
+	controls_text.global_position = Vector2(0, Global.EXPECTED_FLAG_AREA_END_Y + 24)
 	update_score_counter()
 
 

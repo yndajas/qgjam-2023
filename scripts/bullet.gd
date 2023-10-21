14a636a88f673c330fc7d9b31af41cfad22a5067
Ynda Jas <yndajas@gmail.com>
2023-10-21 03:39:31 +0100

    Fix `.gd` lint errors

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,18 +1,18 @@
 extends RigidBody2D
 
+enum Edge { BOTTOM, TOP }
 const EXPECTED_BULLET_EDGE_OFFSET = 8
 const PLAYABLE_BOTTOM_EDGE: int = 720
 const PLAYABLE_TOP_EDGE: int = 0
 const SPRITE_COUNT: int = 6
 const SPRITE_SCALE: int = 4
 @export var fire_sounds: Array[AudioStreamWAV]
+@onready var sfx_player: AudioStreamPlayer = $SfxPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var sprite_size: float = sprite.get_rect().size[0]
 @onready var bullet_size: float = sprite_size * SPRITE_SCALE
 @onready var bullet_edge_offset: float = ceili(bullet_size / 2.0)
-@onready var sfx_player: AudioStreamPlayer = $SfxPlayer
 
-enum Edge { BOTTOM, TOP }
 
 func _ready() -> void:
 	var sprite_index: int = randi() % SPRITE_COUNT
@@ -20,22 +20,28 @@ func _ready() -> void:
 	sprite.scale = Vector2(SPRITE_SCALE, SPRITE_SCALE)
 	play_fire_sound()
 
+
 func _physics_process(_delta: float) -> void:
 	if is_off_screen():
 		queue_free()
 
+
 func is_off_screen() -> bool:
 	return is_off_screen_bottom() or is_off_screen_top()
+
 
 func is_off_screen_bottom() -> bool:
 	return y_position() - bullet_edge_offset > PLAYABLE_BOTTOM_EDGE
 
+
 func is_off_screen_top() -> bool:
 	return y_position() + bullet_edge_offset < PLAYABLE_TOP_EDGE
+
 
 func play_fire_sound() -> void:
 	sfx_player.stream = fire_sounds.pick_random()
 	sfx_player.play()
+
 
 func y_position() -> float:
 	return self.global_position[1]
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
+enum Edge { LEFT, RIGHT }
 const PLAYABLE_LEFT_EDGE: int = 0
 const PLAYABLE_RIGHT_EDGE: int = 1280
 const PLAYER_GUN_OFFSET: int = 40
 const SPEED: float = 600.0
-@onready var player_edge_offset: float = $AnimatedSprite2D.get_sprite_frames().get_frame_texture("default", 0).get_size()[0] / 2.0
-var bullet_scene: PackedScene = preload("res://scenes/bullet.tscn")
 var bullet_cooldown: float = 0
+var bullet_scene: PackedScene = preload("res://scenes/bullet.tscn")
+@onready var player_edge_offset: float = (
+	$AnimatedSprite2D.get_sprite_frames().get_frame_texture("default", 0).get_size()[0] / 2.0
+)
 
-enum Edge { LEFT, RIGHT }
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
@@ -28,26 +30,33 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_released("fire"):
 		bullet_cooldown = 0
 
+
 func fire() -> void:
 	var bullet: RigidBody2D = bullet_scene.instantiate()
-	bullet.global_position = Vector2(x_position(), y_position() - PLAYER_GUN_OFFSET - bullet.EXPECTED_BULLET_EDGE_OFFSET)
+	bullet.global_position = Vector2(
+		x_position(), y_position() - PLAYER_GUN_OFFSET - bullet.EXPECTED_BULLET_EDGE_OFFSET
+	)
 	get_tree().get_root().add_child(bullet)
 	bullet_cooldown = 0.2
+
 
 func has_hit_edge(edge: int) -> bool:
 	if is_left_edge(edge):
 		return x_position() - player_edge_offset < PLAYABLE_LEFT_EDGE
-	else:
-		return x_position() + player_edge_offset > PLAYABLE_RIGHT_EDGE
+
+	return x_position() + player_edge_offset > PLAYABLE_RIGHT_EDGE
+
 
 func is_left_edge(edge: int) -> bool:
 	return edge == Edge.LEFT
+
 
 func reset_if_beyond_edge() -> void:
 	if has_hit_edge(Edge.LEFT):
 		reset_to_edge(Edge.LEFT)
 	elif has_hit_edge(Edge.RIGHT):
 		reset_to_edge(Edge.RIGHT)
+
 
 func reset_to_edge(edge: int) -> void:
 	var target_x_position: float
@@ -59,8 +68,10 @@ func reset_to_edge(edge: int) -> void:
 
 	self.global_position = Vector2(target_x_position, y_position())
 
+
 func x_position() -> float:
 	return self.global_position[0]
+
 
 func y_position() -> float:
 	return self.global_position[1]

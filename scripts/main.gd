0826432361e105982f58c5565c1ec09e075b812f
Ynda Jas <yndajas@gmail.com>
2023-10-21 17:31:50 +0100

    Move `_` method above non-`_` methods

--- scripts/main.gd
+++ scripts/main.gd
@@ -8,6 +8,12 @@ func _ready() -> void:
 	spawn_enemy()
 
 
+func _on_enemy_spawn_timer_timeout() -> void:
+	spawn_enemy()
+	enemy_spawn_timer.wait_time = randf_range(2, 6)
+	enemy_spawn_timer.start()
+
+
 func spawn_enemy() -> void:
 	var enemy: CharacterBody2D = enemy_scene.instantiate()
 	enemy.global_position.y = randf_range(enemy.MINIMUM_SPAWN_Y, enemy.MAXIMUM_SPAWN_Y)
@@ -30,9 +36,3 @@ func set_enemy_direction_left(enemy: CharacterBody2D) -> void:
 func set_enemy_direction_right(enemy: CharacterBody2D) -> void:
 	enemy.global_position.x = Global.PLAYABLE_RIGHT_EDGE + enemy.EXPECTED_EDGE_OFFSET
 	enemy.velocity.x = -enemy.SPEED
-
-
-func _on_enemy_spawn_timer_timeout() -> void:
-	spawn_enemy()
-	enemy_spawn_timer.wait_time = randf_range(2, 6)
-	enemy_spawn_timer.start()

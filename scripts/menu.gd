a8389a3e3244d5685a70641025a3b4ce5bfaaab6
Ynda Jas <yndajas@gmail.com>
2023-10-22 02:25:29 +0100

    Update menu
    - Adjust text layout
    - Pulse player sprite and start game text
    - Rename player sprite node

---
+++ scripts/menu.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var alpha: float = 1.0
+var alpha_decreasing: bool = true
+@onready var player_sprite: Sprite2D = $PlayerSprite
+@onready var start_controls_text: RichTextLabel = $StartControlsText
+
+
+func _physics_process(delta: float) -> void:
+	if alpha == 0:
+		alpha_decreasing = false
+	elif alpha == 1:
+		alpha_decreasing = true
+
+	if alpha_decreasing:
+		alpha = max(alpha - delta / 1.5, 0)
+	else:
+		alpha = min(alpha + delta / 1.5, 1)
+
+	player_sprite.modulate.a = alpha
+	start_controls_text.modulate.a = alpha
